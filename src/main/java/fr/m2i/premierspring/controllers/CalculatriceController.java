ed75af9bef0a52ebb5d2136254ba562396fb33ad
Olivier-Argaud-M2I <Olivier.ARGAUD@labom2iformation.fr>
2022-08-31 18:01:42 +0200

    serialisation des enums

--- src/main/java/fr/m2i/premierspring/controllers/CalculatriceController.java
+++ src/main/java/fr/m2i/premierspring/controllers/CalculatriceController.java
@@ -1,12 +1,17 @@
 package fr.m2i.premierspring.controllers;
 
 
+import fr.m2i.premierspring.enums.OperationType;
 import fr.m2i.premierspring.models.DataModel;
+import fr.m2i.premierspring.models.DataModel2;
 import fr.m2i.premierspring.services.CalculatriceService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cal")
@@ -27,7 +32,8 @@ public class CalculatriceController {
 
 
     @GetMapping(
-            value="/result2"
+            value="/result2",
+            produces = MediaType.APPLICATION_JSON_VALUE
     )
     public DataModel getResult2(
             @RequestParam("a")Double a,
@@ -38,7 +44,8 @@ public class CalculatriceController {
     }
 
     @GetMapping(
-            value="/result3/{a}/{b}/{operation}"
+            value="/result3/{a}/{b}/{operation}",
+            produces = MediaType.APPLICATION_JSON_VALUE
     )
     public DataModel getResult3(
             @PathVariable("a")Double a,
@@ -48,4 +55,47 @@ public class CalculatriceController {
         return calculatriceService.calculate(new DataModel(operation,a,b));
     }
 
+
+
+    @PostMapping(
+            value = "/result4",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<DataModel> getResult4(@RequestBody List<DataModel> dataModels){
+        List<DataModel>modelList = new ArrayList<>();
+        for (DataModel dataModel:dataModels) {
+            modelList.add(calculatriceService.calculate(dataModel));
+        }
+        return modelList;
+    }
+
+    @PostMapping(
+            value = "/result5",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    public String getResult5(@RequestBody List<DataModel2> dataModels){
+        StringBuilder result = new StringBuilder();
+        result.append("les resultats sont : ");
+        for (DataModel2 dataModel:dataModels) {
+            DataModel2 reponse = calculatriceService.calculate2(dataModel);
+            result.append(reponse.getResult());
+            result.append(" // ");
+        }
+        return result.toString();
+    }
+
+    @PostMapping(
+            value = "/result6",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<DataModel2> getResult6(@RequestBody List<DataModel2> dataModels){
+        List<DataModel2>modelList = new ArrayList<>();
+        for (DataModel2 dataModel:dataModels) {
+            modelList.add(calculatriceService.calculate2(dataModel));
+        }
+        return modelList;
+    }
 }
--- src/main/java/fr/m2i/premierspring/enums/OperationType.java
+++ src/main/java/fr/m2i/premierspring/enums/OperationType.java
@@ -1,12 +1,23 @@
 package fr.m2i.premierspring.enums;
 
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.apache.tomcat.util.buf.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public enum OperationType {
 
-    ADDITION,
-    SOUSTRACTION,
-    MULTIPLICATION,
-    DIVISION,
-    PUISSANCE;
+
+
+
+    ADDITION("ADDITION"),
+    SOUSTRACTION("SOUSTRACTION"),
+    MULTIPLICATION("MULTIPLICATION"),
+    DIVISION("DIVISION"),
+    PUISSANCE("PUISSANCE");
 
     public static OperationType fromString(String operation){
         switch (operation){
@@ -19,6 +30,51 @@ public enum OperationType {
         }
 
     }
+
+    OperationType(String value) {
+        this.value = value;
+    }
+
+    private String value;
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    //    public enum DeviceScheduleFormat {
+//
+//        Weekday,
+//        EvenOdd,
+//        Interval;
+//
+//        private static Map<String, OperationType> namesMap = new HashMap<String, OperationType>(5);
+//
+//        static {
+//            namesMap.put("ADDITION", ADDITION);
+//            namesMap.put("SOUSTRACTION", SOUSTRACTION);
+//            namesMap.put("MULTIPLICATION", MULTIPLICATION);
+//            namesMap.put("DIVISION", DIVISION);
+//            namesMap.put("PUISSANCE", PUISSANCE);
+//        }
+//
+//        @JsonCreator
+//        public static OperationType forValue(String value) {
+//            return namesMap.get(StringUtils.lowerCase(value));
+//        }
+//
+//        @JsonValue
+//        public String toValue() {
+//            for (Map.Entry<String, OperationType> entry : namesMap.entrySet()) {
+//                if (entry.getValue() == this)
+//                    return entry.getKey();
+//            }
+//
+//            return null; // or fail
+//        }
+//    }
 
 
 
